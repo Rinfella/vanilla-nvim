f8e00ebfd6fe64a22f4bde1a0b0622d34cbb8b50
Rinfella <rinfellapachuau93@gmail.com>
2025-04-08 15:12:48 +0530

    added render markdown

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,50 +8,51 @@ require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup()
 
 cmp.setup {
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert {
-		['<C-n>'] = cmp.mapping.select_next_item(),
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<M-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = cmp.mapping(function (fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, {'i', 's'}),
-		['<S-Tab>'] = cmp.mapping(function (fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {'i', 's'}),
-	},
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-		{ name = 'buffer' },
-		{ name = 'path' },
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<M-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'render-markdown' },
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     performance = {
         max_view_entries = 8,
     },
@@ -60,8 +61,8 @@ cmp.setup {
         keyword_length = 2,
     },
     formatting = {
-        fields = { "abbr", "kind", "menu" },  -- Define what fields to display
-        expandable_indicator = true,  -- Show expandable indicator
+        fields = { "abbr", "kind", "menu" }, -- Define what fields to display
+        expandable_indicator = true,         -- Show expandable indicator
         format = function(entry, vim_item)
             local kind_icons = {
                 Text = "",
@@ -92,7 +93,7 @@ cmp.setup {
             }
 
             -- Ensure a valid kind icon exists
-            local icon = kind_icons[vim_item.kind] or ""  -- "" as fallback
+            local icon = kind_icons[vim_item.kind] or "" -- "" as fallback
 
             -- Correctly set the kind without breaking cmp.Entry type
             vim_item.kind = string.format("%s %s", icon, vim_item.kind)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,106 +3,108 @@
 -- Bootstrap lazy.nvim if it does not exist
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-    	"--filter=blob:none",
-		"git@github.com:folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "git@github.com:folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load Plugins
 require("lazy").setup({
-	-- Core plugins
-	-- Mason + LSP
-	{
-		"williamboman/mason.nvim",
-		event = "VeryLazy",
-	},
+    -- Core plugins
+    -- Mason + LSP
+    {
+        "williamboman/mason.nvim",
+        event = "VeryLazy",
+    },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-		},
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+        },
 
 
-		config = function()
-		require("plugins.lsp")
-		end,
-	},
+        config = function()
+            require("plugins.lsp")
+        end,
+    },
 
     -- Neovim syntax
     {
         "folke/neodev.nvim",
     },
 
-	-- Comments toggle
-	{
-		"numToStr/Comment.nvim",
-		 event = "VeryLazy",
-		 config = function() require("Comment").setup() end
-	},
-
-    -- TODO Comments
-    -- PERF: 
-    -- HACK:
-    -- FIX: 
-    -- TODO:
-    -- NOTE: 
-    -- WARNING: 
-
+    -- Comments toggle
     {
-      "folke/todo-comments.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      opts = {}
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        config = function() require("Comment").setup() end
     },
 
-	-- Theme and UI
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("plugins.ui")
-		end,
-	},
+    -- TODO Comments
+    -- PERF:
+    -- HACK:
+    -- FIX:
+    -- TODO:
+    -- NOTE:
+    -- WARNING:
+
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {}
+    },
+
+    -- Theme and UI
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("plugins.ui")
+        end,
+    },
 
     -- Dashboard Alpha
     {
-        'goolord/alpha-nvim',
-        config = function ()
-            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        "goolord/alpha-nvim",
+        config = function()
+            require("alpha").setup(
+                require "alpha.themes.dashboard".config
+            )
         end
     },
 
-	-- Status Line
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons"
-		},
-	},
+    -- Status Line
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
 
-	-- Completion
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-		      "L3MON4D3/LuaSnip",
-		      "saadparwaiz1/cmp_luasnip",
-		      "rafamadriz/friendly-snippets",
-		      "hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			require("plugins.cmp")
-		end,
-	},
+    -- Completion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            require("plugins.cmp")
+        end,
+    },
 
     -- Auto Format
     {
@@ -113,181 +115,202 @@ require("lazy").setup({
         end
     },
 
-    -- Auto Save 
+    -- Auto Save
     {
         "https://git.sr.ht/~nedia/auto-save.nvim",
         event = { "BufReadPre" },
         opts = {
-          events = { 'InsertLeave', 'BufLeave' },
-          silent = false,
-          exclude_ft = { 'neo-tree' },
+            events = { 'InsertLeave', 'BufLeave' },
+            silent = false,
+            exclude_ft = { 'neo-tree' },
         },
         config = function()
-           require("plugins.autosave")
+            require("plugins.autosave")
         end,
     },
 
     -- SCRETCH
     {
-        '0xJohnnyboy/scretch.nvim',
-        dependencies = { 'nvim-telescope/telescope.nvim' },
+        "0xJohnnyboy/scretch.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim" },
         config = function()
             require("plugins.scretch")
             require("scretch").setup {}
         end,
     },
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-			}
-		},
-		config = function()
-			require("plugins.telescope")
-		end,
-	},
+    -- Render Markdown
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            completions = {
+                lsp = { enabled = true },
+            },
+        },
+    },
 
-	-- Tree Sitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-			require("plugins.treesitter")
-		end,
-	},
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            }
+        },
+        config = function()
+            require("plugins.telescope")
+        end,
+    },
 
-	-- File Explorer (Neotree)
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		cmd = "Neotree",
-		config = function()
+    -- Tree Sitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("plugins.treesitter")
+        end,
+    },
+
+    -- File Explorer (Neotree)
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        cmd = "Neotree",
+        config = function()
             require("nvim-web-devicons").setup { default = true }
-			require("core.keymaps")
-			require("neo-tree").setup({
-				close_if_last_window = false,
-				popup_border_style = "rounded",
-				enable_git_status = true,
-				enable_diagnostics = true,
-				open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
-				sort_case_insensitive = false,
-				filesystem = {
-					filtered_items = {
-						visible = false,
-						hide_dotfiles = true,
-						hide_gitignored = true,
-						hide_hidden = true,
-					},
-					follow_current_file = {
-						enabled = true,
-					},
-					hijack_netrw_behavior = "open_default",
-				},
-				window = {
-					position = "left",
-					width = 30,
-				},
-			})
-		end,
-	},
+            require("core.keymaps")
+            require("neo-tree").setup({
+                close_if_last_window = false,
+                popup_border_style = "rounded",
+                enable_git_status = true,
+                enable_diagnostics = true,
+                open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+                sort_case_insensitive = false,
+                filesystem = {
+                    filtered_items = {
+                        visible = false,
+                        hide_dotfiles = true,
+                        hide_gitignored = true,
+                        hide_hidden = true,
+                    },
+                    follow_current_file = {
+                        enabled = true,
+                    },
+                    hijack_netrw_behavior = "open_default",
+                },
+                window = {
+                    position = "left",
+                    width = 30,
+                },
+            })
+        end,
+    },
 
     -- TROUBLE
     {
-          "folke/trouble.nvim",
-          opts = {}, -- for default options, refer to the configuration section for custom setup.
-          cmd = "Trouble",
-          keys = {
+        "folke/trouble.nvim",
+        opts = {},
+        cmd = "Trouble",
+        keys = {
             {
-              "<leader>xx",
-              "<cmd>Trouble diagnostics toggle<cr>",
-              desc = "Diagnostics (Trouble)",
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
             },
             {
-              "<leader>xX",
-              "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-              desc = "Buffer Diagnostics (Trouble)",
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
             },
             {
-              "<leader>xs",
-              "<cmd>Trouble symbols toggle focus=false<cr>",
-              desc = "Symbols (Trouble)",
+                "<leader>xs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
             },
             {
-              "<leader>xl",
-              "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-              desc = "LSP Definitions / references / ... (Trouble)",
+                "<leader>xl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
             },
             {
-              "<leader>xL",
-              "<cmd>Trouble loclist toggle<cr>",
-              desc = "Location List (Trouble)",
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
             },
             {
-              "<leader>xQ",
-              "<cmd>Trouble qflist toggle<cr>",
-              desc = "Quickfix List (Trouble)",
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
             },
-          },
+        },
     },
 
     -- AutoPairs
     {
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         event = "InsertEnter",
         config = true
         -- use opts = {} for passing setup options
         -- this is equivalent to setup({}) function
     },
 
-      -- CHEATSHEET NVIM
+    -- CHEATSHEET NVIM
     {
         "doctorfree/cheatsheet.nvim",
         event = "VeryLazy",
         dependencies = {
-          { "nvim-telescope/telescope.nvim" },
-          { "nvim-lua/popup.nvim" },
-          { "nvim-lua/plenary.nvim" },
+            { "nvim-telescope/telescope.nvim" },
+            { "nvim-lua/popup.nvim" },
+            { "nvim-lua/plenary.nvim" },
         },
         config = function()
-          local ctactions = require "cheatsheet.telescope.actions"
-          require("cheatsheet").setup {
-            bundled_cheetsheets = {
-              enabled = { "default", "lua", "markdown", "regex", "netrw", "unicode" },
-              disabled = { "nerd-fonts" },
-            },
-            bundled_plugin_cheatsheets = {
-              enabled = {
-                "auto-session",
-                "goto-preview",
-                "octo.nvim",
-                "telescope.nvim",
-                "vim-easy-align",
-                "vim-sandwich",
-              },
-              disabled = { "gitsigns" },
-            },
-            include_only_installed_plugins = true,
-            telescope_mappings = {
-              ["<CR>"] = ctactions.select_or_fill_commandline,
-              ["<A-CR>"] = ctactions.select_or_execute,
-              ["<C-Y>"] = ctactions.copy_cheat_value,
-              ["<C-E>"] = ctactions.edit_user_cheatsheet,
-            },
-          }
-      end,
+            local ctactions = require "cheatsheet.telescope.actions"
+            require("cheatsheet").setup {
+                bundled_cheetsheets = {
+                    enabled = {
+                        "default",
+                        "lua",
+                        "markdown",
+                        "regex",
+                        "netrw",
+                        "unicode",
+                    },
+
+                    disabled = { "nerd-fonts" },
+                },
+                bundled_plugin_cheatsheets = {
+                    enabled = {
+                        "auto-session",
+                        "goto-preview",
+                        "octo.nvim",
+                        "telescope.nvim",
+                        "vim-easy-align",
+                        "vim-sandwich",
+                    },
+                    disabled = { "gitsigns" },
+                },
+                include_only_installed_plugins = true,
+                telescope_mappings = {
+                    ["<CR>"] = ctactions.select_or_fill_commandline,
+                    ["<A-CR>"] = ctactions.select_or_execute,
+                    ["<C-Y>"] = ctactions.copy_cheat_value,
+                    ["<C-E>"] = ctactions.edit_user_cheatsheet,
+                },
+            }
+        end,
     },
 
 })
-
--- WARN: 
--- BUG: 
