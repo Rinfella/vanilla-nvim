a71aa330993058a232e0702544794823e4d21a3e
Rinfella Pachuau <rinfellapachuau93@gmail.com>
2026-02-03 12:48:43 +0530

    fixed copilot keybind error screaming

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,108 +1,134 @@
+-- Copilot.lua
+
 local M = {}
 
 function M.setup()
-    local loaded = false
-    local timer = vim.loop.new_timer()
-    local retry_interval = 60 * 1000 -- 60s
-    local timeout = 20             -- seconds
+	local loaded = false
+	local timer = vim.loop.new_timer()
+	local retry_interval = 60 * 1000 -- 60s
+	local timeout = 20 -- seconds
 
-    local function load_copilot()
-        if loaded then return end
-        loaded = true
+	local function load_copilot()
+		if loaded then
+			return
+		end
+		loaded = true
 
-        local opts = {
-            panel = { enabled = false },
-            suggestion = {
-                auto_trigger = true,
-                debounce = 75,
-                keymap = {
-                    accept = "<M-a>",
-                    accept_word = "<M-w>",
-                    accept_line = "<M-l>",
-                    next = "<M-]>",
-                    prev = "<M-[>",
-                    dismiss = "<M-/>",
-                },
-            },
-            filetypes = {
-                ["*"] = true,
-            },
-        }
+		local opts = {
+			panel = { enabled = false },
+			suggestion = {
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					-- Disable built-in keymapping to prevent E31 errors
+					-- We will map these manually below
+					accept = false,
+					accept_word = false,
+					accept_line = false,
+					next = false,
+					prev = false,
+					dismiss = false,
+				},
+			},
+			filetypes = {
+				["*"] = true,
+			},
+		}
 
-        require("copilot").setup(opts)
+		require("copilot").setup(opts)
 
-        -- nvim-cmp and luasnip integration
-        local cmp = require("cmp")
-        local copilot = require("copilot.suggestion")
-        local luasnip = require("luasnip")
+		-- Manually set keymaps to avoid the "Could not unset keymap" bug
+		local suggestion = require("copilot.suggestion")
+		local map = vim.keymap.set
 
-        local function set_trigger(trigger)
-            vim.b.copilot_suggestion_auto_trigger = trigger
-            vim.b.copilot_suggestion_hidden = not trigger
-        end
+		-- Only run the command if the suggestion is visible
+		local function suggestion_action(fn)
+			return function()
+				if suggestion.is_visible() then
+					fn()
+				end
+			end
+		end
 
-        -- Disable Copilot when cmp menu is open
-        cmp.event:on("menu_opened", function()
-            if copilot.is_visible() then
-                copilot.dismiss()
-            end
-            set_trigger(false)
-        end)
+		map("i", "<M-a>", suggestion_action(suggestion.accept), { desc = "[Copilot] Accept" })
+		map("i", "<M-w>", suggestion_action(suggestion.accept_word), { desc = "[Copilot] Accept Word" })
+		map("i", "<M-l>", suggestion_action(suggestion.accept_line), { desc = "[Copilot] Accept Line" })
+		map("i", "<M-]>", suggestion_action(suggestion.next), { desc = "[Copilot] Next Suggestion" })
+		map("i", "<M-[>", suggestion_action(suggestion.prev), { desc = "[Copilot] Prev Suggestion" })
+		map("i", "<M-/>", suggestion_action(suggestion.dismiss), { desc = "[Copilot] Dismiss" })
 
-        -- Re-enable Copilot when cmp closes, if not in a snippet
-        cmp.event:on("menu_closed", function()
-            set_trigger(not luasnip.expand_or_locally_jumpable())
-        end)
+		-- nvim-cmp and luasnip integration
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-        -- Also disable when inside snippet nodes
-        vim.api.nvim_create_autocmd("User", {
-            pattern = { "LuasnipInsertNodeEnter", "LuasnipInsertNodeLeave" },
-            callback = function()
-                set_trigger(not luasnip.expand_or_locally_jumpable())
-            end,
-        })
+		local function set_trigger(trigger)
+			vim.b.copilot_suggestion_auto_trigger = trigger
+			vim.b.copilot_suggestion_hidden = not trigger
+		end
 
-        vim.notify("[copilot] Connected and loaded ✅", vim.log.levels.INFO)
-    end
+		-- Disable Copilot when cmp menu is open
+		cmp.event:on("menu_opened", function()
+			if suggestion.is_visible() then
+				suggestion.dismiss()
+			end
+			set_trigger(false)
+		end)
 
-    local function check_api()
-        local stdout = vim.loop.new_pipe(false)
-        local stderr = vim.loop.new_pipe(false)
+		-- Re-enable Copilot when cmp closes, if not in a snippet
+		cmp.event:on("menu_closed", function()
+			set_trigger(not luasnip.expand_or_locally_jumpable())
+		end)
 
-        local handle
-        handle = vim.loop.spawn("curl", {
-            args = {
-                "--max-time", tostring(timeout),
-                "--silent",
-                "--fail",
-                "https://copilot-proxy.githubusercontent.com/_ping"
-            },
-            stdio = { nil, stdout, stderr },
-        }, function(code)
-            stdout:close()
-            stderr:close()
-            handle:close()
+		-- Also disable when inside snippet nodes
+		vim.api.nvim_create_autocmd("User", {
+			pattern = { "LuasnipInsertNodeEnter", "LuasnipInsertNodeLeave" },
+			callback = function()
+				set_trigger(not luasnip.expand_or_locally_jumpable())
+			end,
+		})
 
-            if code == 0 then
-                vim.schedule(load_copilot)
-                timer:stop()
-            else
-                vim.schedule(function()
-                    vim.notify("[copilot] API unreachable. Retrying in 60s…", vim.log.levels.WARN)
-                end)
-            end
-        end)
-    end
+		vim.notify("[copilot] Connected and loaded ✅", vim.log.levels.INFO)
+	end
 
-    vim.defer_fn(check_api, 1000)
+	local function check_api()
+		local stdout = vim.loop.new_pipe(false)
+		local stderr = vim.loop.new_pipe(false)
 
-    timer:start(retry_interval, retry_interval, function()
-        if not loaded then
-            check_api()
-        else
-            timer:stop()
-        end
-    end)
+		local handle
+		handle = vim.loop.spawn("curl", {
+			args = {
+				"--max-time",
+				tostring(timeout),
+				"--silent",
+				"--fail",
+				"https://copilot-proxy.githubusercontent.com/_ping",
+			},
+			stdio = { nil, stdout, stderr },
+		}, function(code)
+			stdout:close()
+			stderr:close()
+			handle:close()
+
+			if code == 0 then
+				vim.schedule(load_copilot)
+				timer:stop()
+			else
+				vim.schedule(function()
+					vim.notify("[copilot] API unreachable. Retrying in 60s…", vim.log.levels.WARN)
+				end)
+			end
+		end)
+	end
+
+	vim.defer_fn(check_api, 1000)
+
+	timer:start(retry_interval, retry_interval, function()
+		if not loaded then
+			check_api()
+		else
+			timer:stop()
+		end
+	end)
 end
 
 return M
